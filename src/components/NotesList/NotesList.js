5e847b65f520a85bbd5a83fddf3c87664552e910
vjespinoza <vjespinoza84@gmail.com>
2021-04-22 16:36:06 -0500

    MagicGrid implemented correctly & Basic note creation: id, time stamp, title and body

--- src/components/NotesList/NotesList.js
+++ src/components/NotesList/NotesList.js
@@ -1,8 +1,8 @@
-// import { useEffect } from "react";
+import { useEffect } from "react";
 import Note from "../Note/Note";
 import { Container } from "@material-ui/core";
 import useStyles from "./style";
-// import MagicGrid from "magic-grid";
+import MagicGrid from "magic-grid";
 
 const NotesList = ({ notes }) => {
     const classes = useStyles();
@@ -12,24 +12,24 @@ const NotesList = ({ notes }) => {
 
     console.log(notes.length);
 
-    // useEffect(() => {
-    //     if (notes.length >= 1) {
-    //         let magicGrid = new MagicGrid({
-    //             container: "#container", // Required. Can be a class, id, or an HTMLElement
-    //             static: false, // Required for static content. Default: false.
-    //             items: 30, // Required for dynamic content. Initial number of items in the container.
-    //             gutter: 30, // Optional. Space between items. Default: 25(px).
-    //             maxColumns: 5, // Optional. Maximum number of columns. Default: Infinite.
-    //             useMin: true, // Optional. Prioritize shorter columns when positioning items. Default: false.
-    //             useTransform: true, // Optional. Position items using CSS transform. Default: True.
-    //             animate: true, // Optional. Animate item positioning. Default: false.
-    //         });
-    //         magicGrid.listen();
-    //         magicGrid.positionItems();
-    //     }
-    // }, [notes]);
+    useEffect(() => {
+        if (notes.length >= 1) {
+            let magicGrid = new MagicGrid({
+                container: "#container",
+                static: false,
+                items: notes.length,
+                gutter: 15,
+                maxColumns: 6,
+                useMin: true,
+                useTransform: true,
+                animate: true,
+            });
+            magicGrid.listen();
+            magicGrid.positionItems();
+        }
+    }, [notes]);
 
-    if (notes.length > 0) {
+    if (notes.length >= 1) {
         return (
             <Container id="container" className={classes.notesListContainer}>
                 {sortedNotes.map((note) => {
